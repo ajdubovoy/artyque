a10fca25b48e1e8ce87dff1d990f4cb754d04993
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-05-20 16:22:41 +0200

    start rendering form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -74,7 +74,7 @@ SimpleForm.setup do |config|
   config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = 'btn'
+  config.button_class = 'button-primary'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
@@ -161,7 +161,7 @@ SimpleForm.setup do |config|
   # config.cache_discovery = !Rails.env.development?
 
   # Default class for inputs
-  # config.input_class = nil
+  config.input_class = ''
 
   # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = 'checkbox'
