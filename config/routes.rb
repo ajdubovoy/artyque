9119470997235ea1894a11bd721a6104883bb95d
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-06-24 17:47:18 +0200

    fix up login form and make it real

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root to: 'pages#home'
   resources :contact_forms, only: :create
   get 'contact_forms/success', to: 'contact_forms#success', as: :contact_forms_success
