bd5d936fbe992014f9c17679a62b74f70b19d95a
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-06-24 15:37:46 +0200

    further migrations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,8 +7,8 @@ class Artist < ApplicationRecord
   has_many :collections
   has_many :artworks, through: :collections
   enum color_palette: %i[ plain angular curved ]
-  validates :color_palette, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
   validates :layout, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
+  validates :shape, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
   validates :button_text, length: { maximum: 30 }
   validates_with DescriptionWordLengthValidator
 end
