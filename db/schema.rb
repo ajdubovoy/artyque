29b201574e2fd9cead0849c821b0e07abe5db7ef
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-06-24 15:38:48 +0200

    add name to artists because they probably need it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190624131920) do
+ActiveRecord::Schema.define(version: 20190624133758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20190624131920) do
     t.string "about_me"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_artists_on_user_id"
   end
 
