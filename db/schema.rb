c87e8df48cbfe3f1273641853d2bbe7cbf715aa3
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-06-25 12:33:51 +0200

    fix up schema after remigrating

--- db/schema.rb
+++ db/schema.rb
@@ -17,13 +17,13 @@ ActiveRecord::Schema.define(version: 20190625082402) do
 
   create_table "artists", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "color_palette", default: 1
-    t.integer "shape", default: 0
+    t.integer "color_palette", default: 0
+    t.integer "shape", default: 1
     t.string "super_title"
     t.string "title"
     t.string "description"
     t.string "button_text"
-    t.integer "layout", default: 0
+    t.integer "layout", default: 1
     t.string "video"
     t.string "quotation"
     t.string "quotation_caption"
