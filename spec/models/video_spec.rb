de1dde3bfb8ef943572f196f8f4f960779a015be
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-07-22 23:37:46 +0200

    add class methods to video to determine whether video is youtube or vimeo

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#vimeo?' do
+    it 'returns true when link is vimeo' do
+      artist = create(:artist)
+      video = create(:video, artwork: artist.artworks.sample)
+      actual = video.vimeo?
+      expect(actual).to be true
+    end
+
+    it 'returns false when link is youtube' do
+      artist = create(:artist)
+      video = create(:video, :youtube, artwork: artist.artworks.sample)
+      actual = video.vimeo?
+      expect(actual).to be false
+    end
+  end
+
+  describe '#youtube?' do
+    it 'returns false when link is vimeo' do
+      artist = create(:artist)
+      video = create(:video, artwork: artist.artworks.sample)
+      actual = video.youtube?
+      expect(actual).to be false
+    end
+
+    it 'returns true when link is youtube' do
+      artist = create(:artist)
+      video = create(:video, :youtube, artwork: artist.artworks.sample)
+      actual = video.youtube?
+      expect(actual).to be true
+    end
+  end
+
+  describe '#type' do
+    it 'returns vimeo when link is vimeo' do
+      artist = create(:artist)
+      video = create(:video, artwork: artist.artworks.sample)
+      actual = video.type
+      expect(actual).to be :vimeo
+    end
+
+    it 'returns youtube when link is youtube' do
+      artist = create(:artist)
+      video = create(:video, :youtube, artwork: artist.artworks.sample)
+      actual = video.type
+      expect(actual).to be :youtube
+    end
+  end
 end
