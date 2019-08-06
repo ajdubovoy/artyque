147867ab4c3d19381c5bf30e51ba5c41304fac53
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-06 16:54:38 +0200

    get stim modal to display white overlay

--- app/javascript/packs/controllers/modal_controller.js
+++ app/javascript/packs/controllers/modal_controller.js
@@ -1,23 +1,42 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "modal", "content" ]
+  static targets = [ "modal", "content", "backdrop" ]
 
   initialize() {
-    this.closeModal();
+    this.modalTarget.classList.add('d-none');
+    this.backdropTarget.classList.add('d-none');
+    this.contentTarget.innerHTML = '';
   }
 
   closeModal() {
     this.modalTarget.classList.add('d-none');
+    this.backdropTarget.classList.add('d-none');
     this.contentTarget.innerHTML = '';
+    const top = document.body.style.top;
+    const scrollStyle = document.querySelector('html').style.scrollBehavior;
+    document.querySelector('html').style.scrollBehavior = 'auto'; // Prevent smooth scrolling
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(top || '0') * -1);
+    document.querySelector('html').style.scrollBehavior = scrollStyle; // Reenable smooth scrolling if enabled
   }
 
   openModal() {
+    const top = window.scrollY;
     this.modalTarget.classList.remove('d-none');
+    this.backdropTarget.classList.remove('d-none');
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${top}px`;
   }
 
   update(event) {
     event.preventDefault();
     this.openModal();
+  }
+
+  hide(event) {
+    event.preventDefault();
+    this.closeModal();
   }
 }
