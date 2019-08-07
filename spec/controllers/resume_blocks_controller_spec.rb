7412af8aa620b08d249d0eea091f9ef6c5c02b4e
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-07 13:42:37 +0200

    add tests for resume blocks controller

--- spec/controllers/resume_blocks_controller_spec.rb
+++ spec/controllers/resume_blocks_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ResumeBlocksController, type: :controller do
+  describe '#index' do
+    it 'renders the right template' do
+      @artist = create(:artist)
+      get :index, params: { artist_id: @artist.id }
+      expect(response).to render_template :index
+    end
 
+    it 'selects the correct artist and blocks' do
+      @artist = create(:artist)
+      get :index, params: { artist_id: @artist.id }
+      expect(assigns(:artist)).to be_instance_of Artist
+      expect(assigns(:artist)).to eq @artist
+      expect(assigns(:blocks)).to eq @artist.blocks
+    end
+
+    context 'xhr for modal' do
+      it 'renders a partial for modal' do
+        @artist = create(:artist)
+        get :index, params: { artist_id: @artist.id }, xhr: true
+        expect(JSON.parse(response.body).has_key? 'html').to be true
+      end
+    end
+  end
 end
