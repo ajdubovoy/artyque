6216db303dd2c07bc229f09bdd55872abec829ea
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-07 13:28:52 +0200

    get resume page looking mostly ok

--- app/controllers/resume_blocks_controller.rb
+++ app/controllers/resume_blocks_controller.rb
@@ -1,6 +1,7 @@
 class ResumeBlocksController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index] # WILL EVENTUALLY BE DISABLED WHEN PROFILES CLOSED
   after_action :skip_authorization, only: %i[index] # WILL EVENTUALLY BE DISABLED WHEN PROFILES CLOSED
+  include MarkdownHelper
   def index
     # For modal
     @artist = Artist.find(params[:artist_id]) # Not authorized
