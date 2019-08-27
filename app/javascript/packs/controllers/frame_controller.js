c76de2577ee5cde303734372b3aa0cf606d57d13
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-27 15:29:10 +0200

    get frame working

--- app/javascript/packs/controllers/frame_controller.js
+++ app/javascript/packs/controllers/frame_controller.js
@@ -3,9 +3,19 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ 'button', 'fixed', 'panel', 'frame', 'nav' ]
 
-  initialize() {
+  connect() {
+    this.toggleFrame();
+    this.buttonTarget.addEventListener('click', this.toggle);
+  }
+
+  disconnect() {
+    this.buttonTarget.removeEventListener('click', this.toggle);
+  }
+
+  toggleFrame() {
     this.fixedTarget.classList.toggle('col-sm-4');
-    this.frameTarget.classList.toggle('d-none');
+    this.frameTarget.classList.toggle('d-sm-none');
+    this.frameTarget.classList.toggle('d-sm-block');
     this.frameTarget.classList.toggle('col-sm-8');
     this.navTarget.classList.toggle('col-sm-3');
     this.panelTarget.classList.toggle('col-sm-9');
@@ -13,15 +23,8 @@ export default class extends Controller {
     this.buttonTarget.classList.toggle('button-tertiary');
   }
 
-  toggle(event) {
+  toggle = (event) => {
     event.preventDefault(); 
-    this.fixedTarget.classList.toggle('col-sm-4');
-    this.frameTarget.classList.toggle('d-none');
-    this.frameTarget.classList.toggle('col-sm-8');
-    this.navTarget.classList.toggle('col-sm-3');
-    this.panelTarget.classList.toggle('col-sm-9');
-    console.log('here')
-    this.buttonTarget.classList.toggle('button-primary');
-    this.buttonTarget.classList.toggle('button-tertiary');
+    this.toggleFrame();
   }
 }
