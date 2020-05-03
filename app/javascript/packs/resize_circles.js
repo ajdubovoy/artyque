e43327f3c5c7946a63267eca13815d4f67657d02
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-05-03 14:44:09 +0200

    sets image with cricles for footer banners

--- app/javascript/packs/resize_circles.js
+++ app/javascript/packs/resize_circles.js
@@ -2,16 +2,16 @@ const resizeCircles = () => {
   const resize = (e) => {
     const navBar = document.querySelector('.new-look-banner')
     const topCircle = document.querySelector('.top-right-circle')
-    const containerWidth = navBar.offsetWidth
-    const containerHeight = navBar.offsetHeight
-    const top = (containerWidth / - 2.63)
-    topCircle.style.top = `${top}px`
+    if (topCircle) {
+      const containerWidth = navBar.offsetWidth
+      const containerHeight = navBar.offsetHeight
+      const top = (containerWidth / - 2.63)
+      topCircle.style.top = `${top}px`
 
-    const botCircle = document.querySelector('.bot-left-circle')
-    // const topTwo = ((containerHeight / 2.63))
-    // botCircle.style.top = `${topTwo}px`
-
-
+      const botCircle = document.querySelector('.bot-left-circle')
+      // const topTwo = ((containerHeight / 2.63))
+      // botCircle.style.top = `${topTwo}px`
+    }
   }
   window.addEventListener('resize', resize)
 }
