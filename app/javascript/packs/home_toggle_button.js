d98052939e80e4405a4818c51135124f6dbd57ad
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-05-29 20:58:59 +0200

    implements tab changing on toggle change

--- app/javascript/packs/home_toggle_button.js
+++ app/javascript/packs/home_toggle_button.js
@@ -2,9 +2,13 @@ const moveBall = () => {
   const ball = document.querySelector('.ball')
   const forArtists = document.querySelector('.sides.left')
   const forCollectors = document.querySelector('.sides.right')
+  const artistTab = document.querySelector('.artist-tab')
+  const collectorTab = document.querySelector('.collector-tab')
   ball.classList.toggle('active')
   forArtists.classList.toggle('active')
   forCollectors.classList.toggle('active')
+  artistTab.classList.toggle('active')
+  collectorTab.classList.toggle('active')
 };
 const toggleHomePageButton = () => {
   const toggler = document.querySelector('.toggle-button')
