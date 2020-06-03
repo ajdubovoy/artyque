cedaa76241871722fdbf0f78ce9855e07d0b138e
Alexander Dubovoy <ajdubovoy@gmail.com>
2020-06-03 09:15:30 +0200

    include some comments

--- app/javascript/packs/circle_divs.js
+++ app/javascript/packs/circle_divs.js
@@ -4,7 +4,7 @@ const initCircleDivs = () => {
   const circles = document.querySelectorAll('.circle')
   if (circles.length > 0) {
     circles.forEach((circle) => {
-      new CircleType(circle);
+      new CircleType(circle).radius(123 / 2);
     });
     const bannerCircle = document.querySelector('.banner-circle')
     bannerCircle.style.transform = 'rotate(128deg)'
